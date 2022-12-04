69e0b1d88890bb9b0669005f59702bccdc7b0ba7
SergioCoP <84104056+SergioCoP@users.noreply.github.com>
2022-12-03 18:13:01 -0600

    [UPDATE] Agregado angularjs con objetos, validacion de forms

--- controller/landingAjs.js
+++ controller/landingAjs.js
@@ -3,11 +3,26 @@ var app = angular.module("ux",['ngRoute']);
 app.controller("landing", ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
     $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
-    console.log("Hola")
 
-$scope.url = window.location.href;
+// $scope.listCard = []
+// $scope.url = window.location.href;
+// console.log($scope.url)
 
-console.log($scope.url)
+$scope.listCard = [
+    {title: "Microsoft Word",img: "/components/img/landing/OfficeWord.jpeg",description:"Aprende a mejorar el diseño y orden tus documentos"},
+    {title: "Adobe Photoshop",img: "/components/img/landing/Photoshop.jpg",description:"Dale un mejor toque a cada una de las imagenes que desees"},
+    {title: "Arduino",img: "/components/img/landing/arduino.jpg",description:"Aprende desde lo básico para automatizar diversas tareas"},
+    {title: "Blender",img: "/components/img/landing/Blender.jpg",description:"Modela tus ideas en 3D"},
+    {title: "CSS",img: "/components/img/landing/CSS.png",description:"Mejora los diseños de tus paginas con este lenguaje de estilos"},
+    {title: "MySQL",img: "/components/img/landing/mysql.jpg",description:"Deseas guardar informacion de tu app con mysql lo puedes hacer"},
+    {title: "Microsoft Word",img: "/components/img/landing/OfficeWord.jpeg",description:"Aprende a mejorar el diseño y orden tus documentos"},
+    {title: "Adobe Photoshop",img: "/components/img/landing/Photoshop.jpg",description:"Dale un mejor toque a cada una de las imagenes que desees"},
+    {title: "Arduino",img: "/components/img/landing/arduino.jpg",description:"Aprende desde lo básico para automatizar diversas tareas"},
+    {title: "Blender",img: "/components/img/landing/Blender.jpg",description:"Modela tus ideas en 3D"},
+    {title: "CSS",img: "/components/img/landing/CSS.png",description:"Mejora los diseños de tus paginas con este lenguaje de estilos"},
+    {title: "MySQL",img: "/components/img/landing/mysql.jpg",description:"Deseas guardar informacion de tu app con mysql lo puedes hacer"}
+]
+    console.log($scope.listCard)
 ///
     // $rootScope.errorhttp = function (data, status, headers, config) {
     //     switch (Number(status)) {
